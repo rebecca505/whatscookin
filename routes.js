e948b9ab0e0e115a2aa91a8f1c80352ab765b276
Shirley <elenagrenewherd7856@gmail.com>
2024-10-05 17:34:31 -0400

    scrape route works but actual scraping doesn't

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+// for routing
 var express = require("express");
 var router = express.Router();
 
@@ -6,9 +7,35 @@ router.get("/", function(req, res){
     res.render("main");
 })
 
-router.get("/scrape", function(req, res){
-    console.log("scrape");
-    res.render("scrape");
-})
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// define route to render scraped data
+router.get("/scrape", async(req, res) => {
+    console.log("scraping!");
+    try
+    {   
+        // get HTML content of Columbia dining webpage
+        const response = await axios.get("https://dining.columbia.edu/");
+        const html = response.data;
+        // initialize Cheerio with HTML content
+        const $ = cheerio.load(html);
+
+        // extract dining halls from HTML doc
+        const openDiningHalls = [];
+        $(".location.clearfix.dining-location.open .name a").each((index, element) => {
+            openDiningHalls.push($(element).text().trim());
+        });
+
+        console.log("Open dining halls:", openDiningHalls);
+        // send data to scrape.ejs template to display
+        res.render("scrape", { openDiningHalls });
+    }
+    catch (error)
+    {
+        console.error(error);
+        res.status(500).send('Error scraping data');
+    }
+});
 
 module.exports = router; 
