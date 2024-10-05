cf36a8e8eee7d1b56cdd4d699f4483c40ce6b7d2
Shirley <elenagrenewherd7856@gmail.com>
2024-10-05 18:53:29 -0400

    scraped all open dining halls

--- routes.js
+++ routes.js
@@ -28,22 +28,34 @@ router.get("/scrape", async(req, res) => {
             }
         });
 
-        // navigate to Columbia dining webpage
-        await page.goto("https://dining.columbia.edu/", {
-            waitUntil: "domcontentloaded" // ensure DOM is loaded
-        });
+        // function to scrape dining halls from specified URL & selector
+        const scrapeDiningHalls = async (url, selector) => {
+            // navigate to Columbia dining webpage
+            await page.goto(url, { waitUntil: "domcontentloaded" }); // ensure DOM is loaded
+            await page.waitForSelector(selector);
 
-        // scrape dining halls from webpage
-        const openDiningHalls = await page.evaluate(() => {
-            const items = [];
             // select elements containing dining hall names
-            const elements = document.querySelectorAll(".location.clearfix.dining-location.open .name a");
-            elements.forEach(element => {
-                items.push(element.textContent.trim());
-            });
-            return items;
-        });
+            return await page.evaluate((selector) => {
+                const items = [];
+                const elements = document.querySelectorAll(selector);
+                elements.forEach(element => {
+                    items.push(element.textContent.trim());
+                });
+                return items;
+            }, selector);
+        };
 
+        // scrape from Columba & Barnard dining webpages
+        const columbiaHalls = await scrapeDiningHalls("https://dining.columbia.edu/", 
+            `.location.clearfix.dining-location.open .name a, 
+             .location.clearfix.retail-location.open .name a`
+        );
+        const barnardHalls = await scrapeDiningHalls("https://dineoncampus.com/barnard",
+            ".row.whats-open-tile_hours_8qXHw .whats-open-tile_locationName_26Mtj"
+        );
+        
+        // combine data
+        const openDiningHalls = [...columbiaHalls, ...barnardHalls];
         console.log("Open dining halls:", openDiningHalls);
 
         // close the browser
