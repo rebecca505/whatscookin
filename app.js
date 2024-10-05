08399ed6371d95beb49423c3255fc293b4c2f7cd
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2024-10-05 14:14:56 -0400

    Set up server

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+
+var routes = require("./routes");
+var app = express();
+
+app.set("port", process.env.PORT || 1400);
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.use(routes);
+
+app.listen(app.get("port"), function(){
+    console.log("Server started on port " + app.get("port"));
+});
